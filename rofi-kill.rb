6d7ec8054d91428ba237a147a523c635bb8f4a25
Benjamin Schnitzler <bschnitzler@horstmann-it-consulting.de>
2022-04-19 20:02:47 +0200

    script to kill memory consuming apps

---
+++ rofi-kill.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+apps = {
+  teams: 'killall -9 teams',
+  firefox: 'killall -9 firefox',
+  qutebrowser: 'killall -9 qutebrowser',
+  signal: 'killall -9 signal'
+}
+
+chosen = `echo #{apps.keys.join('#')} | rofi -dmenu -sep '#'`
+`#{apps[chosen.chomp.to_sym]}`
